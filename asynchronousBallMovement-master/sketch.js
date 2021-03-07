d30bb19eccb4ea8d577e00bc1d6291a6bf8ff666
MonomitaBhattacharjee <ujjwalabhattacharjee@gmail.com>
2021-03-07 20:37:18 +0530

    player added

--- asynchronousBallMovement-master/sketch.js
+++ asynchronousBallMovement-master/sketch.js
@@ -1,32 +1,22 @@
-var ball;
+var canvas;
 var database;
 var position;
+var gameState = 0;
+var playerCount;
+var form,player,game;
 
 function setup(){
     database = firebase.database()
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+  canvas =  createCanvas(500,500);
 
-    var ballPosition = database.ref("ball/position");
-    ballPosition.on("value",readPosition,showError);
+  game = new Game()
+  game.getState()
+  game.start()
+   
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
+
 }
 
 function changePosition(x,y){
